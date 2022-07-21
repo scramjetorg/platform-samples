d2a66dcd1f6bb304886a847776277563c91126e5
Tomasz Kociołek <tomasz.kociolek@scramjet.org>
2022-07-21 12:49:07 +0000

    feat(write-to-database) moved variables to config file

--- typescript/write-to-database/index.ts
+++ typescript/write-to-database/index.ts
@@ -80,34 +80,35 @@ async function* chunksToLines(chunksAsync: Readable) {
   }
 }
 
-const app: ReadableApp<string, [string, string]> = async function* (input) {
-  const connection = createConnection({
-    host: "sql11.freemysqlhosting.net",
-    user: "sql11507574",
-    password: "ewsLgyjpUn",
-    database: "sql11507574",
-  });
+const app: ReadableApp<string, [string, string, string, string]> =
+  async function* (input, host, user, password, database) {
+    const connection = createConnection({
+      host: this.config.host as string,
+      user: this.config.user as string,
+      password: this.config.password as string,
+      database: this.config.database as string,
+    });
 
-  try {
-    this.logger.debug("Connecting to database");
-    await connect(connection);
-    this.logger.debug("Checking for todos table");
-    await createTodos(connection);
-    this.logger.debug("Awaiting inserts");
+    try {
+      this.logger.debug("Connecting to database");
+      await connect(connection);
+      this.logger.debug("Checking for todos table");
+      await createTodos(connection);
+      this.logger.debug("Awaiting inserts");
 
-    for await (const line of chunksToLines(input)) {
-      try {
-        this.logger.debug(`Insering ${line}`);
-        await insertTodo(connection)(line);
-      } catch (err) {
-        this.logger.error("Error while inserting", err);
+      for await (const line of chunksToLines(input)) {
+        try {
+          this.logger.debug(`Insering ${line}`);
+          await insertTodo(connection)(line);
+        } catch (err) {
+          this.logger.error("Error while inserting", err);
+        }
       }
+    } catch (err) {
+      this.logger.error("Error while starting", err);
+      this.logger.debug("Disconnecting");
+      await disconnect(connection);
     }
-  } catch (err) {
-    this.logger.error("Error while starting", err);
-    this.logger.debug("Disconnecting");
-    await disconnect(connection);
-  }
-};
+  };
 
 export default app;
