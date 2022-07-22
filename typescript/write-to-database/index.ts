9e1f9d301e15ba653c61a501165d0661f82a3bdd
Tomasz Kociołek <tomasz.kociolek@scramjet.org>
2022-07-22 17:03:39 +0000

    feat(write-to-database) moved utils to proper lib files

--- typescript/write-to-database/index.ts
+++ typescript/write-to-database/index.ts
@@ -1,84 +1,12 @@
 import { ReadableApp } from "@scramjet/types";
-import { Connection, createConnection } from "mysql";
-import { PassThrough, Readable } from "stream";
-
-const createTodosQuery = `create table if not exists todos(
-                            id int primary key auto_increment,
-                            title varchar(255) not null,
-                            completed tinyint(1) not null default 0
-                        )`;
-
-const createInsertTodoQuery = (title) =>
-  `insert into todos (title) values ("${title}")`;
-
-const connect = (connection: Connection): Promise<void> => {
-  return new Promise((res, rej) => {
-    connection.connect((err) => {
-      if (err) {
-        rej(err);
-      } else {
-        res();
-      }
-    });
-  });
-};
-
-const createTodos = (connection: Connection): Promise<any> => {
-  return new Promise((res, rej) => {
-    connection.query(createTodosQuery, (err, results, fields) => {
-      if (err) {
-        rej(err);
-      }
-
-      res(results);
-    });
-  });
-};
-
-const insertTodo =
-  (connection: Connection) =>
-  (title: string): Promise<any> => {
-    return new Promise((res, rej) => {
-      connection.query(createInsertTodoQuery(title), (err, results, fields) => {
-        if (err) {
-          rej(err);
-        }
-
-        res(results);
-      });
-    });
-  };
-
-const disconnect = (connection: Connection): Promise<void> => {
-  return new Promise((res, rej) => {
-    connection.end(function (err) {
-      if (err) {
-        rej(err);
-      } else {
-        res();
-      }
-    });
-  });
-};
-
-async function* chunksToLines(chunksAsync: Readable) {
-  let previous = "";
-  for await (const chunk of chunksAsync.pipe(
-    new PassThrough({ encoding: "utf-8" })
-  )) {
-    previous += chunk;
-    let eolIndex;
-    while ((eolIndex = previous.indexOf("\n")) >= 0) {
-      // line includes the EOL
-      const line = previous.slice(0, eolIndex + 1);
-      yield line;
-      previous = previous.slice(eolIndex + 1);
-    }
-  }
-  if (previous.length > 0) {
-    yield previous;
-  }
-}
+import {
+  connect,
+  createConnection,
+  createTodos,
+  disconnect,
+  insertTodo,
+} from "./lib/db";
+import { chunksToLines } from "./lib/utils";
 
 const app: ReadableApp<string, [string, string, string, string]> =
   async function* (input) {
