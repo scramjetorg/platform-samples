c575088ffef34dddbd9a98af349338da94585aaa
a-tylenda <agnieszka.tylenda@scramjet.com>
2022-08-09 13:45:22 +0000

    code comments corrections

--- javascript/pokemon/index.js
+++ javascript/pokemon/index.js
@@ -3,9 +3,9 @@ const { loadPokemon } = require("./utils");
 
 /**
  * Sequence that transforms each pokemon name into a pokemon data
- * 
+ *
  * @param {Stream} input - pokemon name passed as an input stream
- * @returns {String} - pokemon weight on stdout, pokemon data on stdout
+ * @returns {String} - pokemon weight on output, pokemon data on stdout
  */
 module.exports = async function(input) {
     return input.pipe(new Transform({
