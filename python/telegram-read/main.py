a394128a9e6b0fc47528226f7b78b9971066a473
gzukowski <85632612+gzukowski@users.noreply.github.com>
2023-04-19 15:16:13 +0200

    Update main.py

--- python/telegram-read/main.py
+++ python/telegram-read/main.py
@@ -1,7 +1,6 @@
 import asyncio
 from scramjet.streams import Stream
 import requests
-import sys
 
 
 
@@ -10,18 +9,29 @@ provides = {
     'contentType': 'text/plain'
 }
 
+message = {
+	"text": "text",
+	"msg_id":"msg_id",
+	"ch_id":"ch_id"
+}
+
+def message_update(message, result):
+	message["text"] = result[-1]["channel_post"]["text"]
+	message["msg_id"] = result[-1]['channel_post']["message_id"]
+	message["ch_id"] = str(result[-1]['channel_post']['sender_chat']['id'])
+
 async def get_updates(stream):
-	temp = []
 	url = f"https://api.telegram.org/bot{run.TOKEN}/getUpdates?chat_id={run.CHANNEL_ID}"
+	msgid = 0
 	while True:
 		result = requests.get(url).json()['result']
-		result = result[-1]["channel_post"]["text"]
-		if result == "stop":
-			sys.exit()
-			return
-		if result not in temp:
-			temp.append(result)
-			stream.write(result)
+		if result != []:
+			message_update(message, result)
+			if message["msg_id"] != msgid and message["ch_id"] == run.CHANNEL_ID:
+				msgid = message["msg_id"]
+				stream.write(message["text"])
+		else:
+			stream.write("No messages currently available")
 		await asyncio.sleep(5)
 		
 		
@@ -31,5 +41,5 @@ async def run(context, input):
 	stream = Stream()
 	run.TOKEN = config['credentials']['token']
 	run.CHANNEL_ID = config['credentials']['channel']
-	asyncio.create_task(get_updates(stream))
+	asyncio.gather(get_updates(stream), return_exceptions=True)
 	return stream.map(lambda x : x + "\n")
