d911e3b938ed2699d29e7b619f3664a958638260
Piotr <piotrek.falba@gmail.com>
2023-06-09 12:25:47 +0000

    Clockify sample

---
+++ typescript/clockify/index.ts
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+import { ClockifyEntryBuilder } from "./utils/clockifyEntryBuilder";
+import { ReadableApp } from "@scramjet/types";
+
+const app: ReadableApp<string> = async function(
+    _stream,
+    interval:number = 1000 * 60 * 60 * 24,
+    apiKey:string) {
+    this.logger.info("starting instance which adds clockify entry daily");
+    const entryBuilder = new ClockifyEntryBuilder();
+    entryBuilder.setApiKey(apiKey);
+
+    entryBuilder.setDate(new Date());
+    await entryBuilder.sendRequest()
+        .then((res) => this.logger.info(res));
+
+    setInterval(async () => {
+        entryBuilder.setDate(new Date());
+        await entryBuilder.sendRequest()
+            .then((res) => this.logger.info(res));
+    }, interval);
+
+    return new Promise((_res, _rej) => { });
+};
+
+export default app;
