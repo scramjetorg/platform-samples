40b54db0ec606923902eda13fa5a21828e8adb72
gzukowski <gracek_608@wp.pl>
2023-07-05 09:25:11 +0000

    Added suggested changes

--- python/Stripe/main.py
+++ python/Stripe/main.py
@@ -8,42 +8,43 @@ provides = {
     'contentType': 'text/plain'
 }
 
+class Stripe:
+      def __init__(self,stream,stripe_api):
+            self.stream = stream
+            self.stripe_api = stripe_api
+      
+      def get_mail(self, user):
+            return user['data']['object']['email']
+      
+      def get_fullname(self, user):
+            mail = user['data']['object']['email']
+            name = mail.split("@")
+            name = name[0] + " stripe"
+            return name
+      
+      async def get_event(self):
+            await asyncio.sleep(3)
+            compared = stripe.Event.list(type="customer.created")['data'][-1]
+            tako = stripe.Event.list(type="customer.created")['data']
 
-def get_mail(user):
-	return user['data']['object']['email']
-	
-def get_fullname(user):
-	mail = user['data']['object']['email']
-	name = mail.split("@")
-	name = name[0] + " stripe"
-	return name
+            self.stream.write(self.get_mail(compared)+ " " +self.get_fullname(compared))
+            compared = compared['id']
+            while True:
+                  test = stripe.Event.list(type="customer.created", ending_before=compared, limit=3)['data']
 
-async def get_event(stream):
-	await asyncio.sleep(3)
-	compared = stripe.Event.list(type="customer.created")['data'][-1]
-	tako = stripe.Event.list(type="customer.created")['data']
-
-	stream.write(get_mail(compared)+ " " +get_fullname(compared))
-	compared = compared['id']
-	while True:
-		test = stripe.Event.list(type="customer.created", ending_before=compared, limit=3)['data']
-
-		for i in range(len(test)):
-			stream.write(get_mail(test[i]) + " " +get_fullname(test[i]))
-		if len(test) != 0:
-			compared = test[0]['id']
-		await asyncio.sleep(3)
-		
+                  for i in range(len(test)):
+                        self.stream.write(self.get_mail(test[i]) + " " +self.get_fullname(test[i]))
+                  if len(test) != 0:
+                        compared = test[0]['id']
+                  await asyncio.sleep(3)
 
 async def run(context, input):
-	try:
-		stripe.api_key = context.config['stripe_api']
-	except Exception as error:
-		raise Exception(f"Config not loaded: {error}")
-		return
-	stream = Stream()
-	asyncio.gather(get_event(stream), return_exceptions=True)
-	return stream.map(lambda x : x + "\n")
-	
-	
-	
+      stream = Stream()
+      try:
+            stripe.api_key = context.config['stripe_api']
+            stripeReader = Stripe(stream, stripe.api_key)
+      except Exception as error:
+            raise Exception(f"Config not loaded: {error}")
+            return
+      asyncio.gather(stripeReader.get_event(), return_exceptions=True)
+      return stream.map(lambda x : x + "\n")
