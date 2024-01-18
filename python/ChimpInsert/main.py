8f0dc20e176eadc63628f0832f6f6d8c26caf6e4
gzukowski <gracek_608@wp.pl>
2024-01-18 22:18:45 +0000

    Changed the way of getting the refresh token

--- python/ChimpInsert/main.py
+++ python/ChimpInsert/main.py
@@ -1,6 +1,7 @@
 import requests
 import asyncio
 import json
+import enum
 from scramjet.streams import Stream
 import mailchimp_marketing
 from mailchimp_marketing import Client
@@ -12,6 +13,12 @@ requires = {
 }
 
 WAIT_TIME_ERROR = 3
+
+class TopicInfo(enum.Enum):
+      STRIPE = "stripe"
+      AUTH0 = "auth0-user"
+      A0_NEWSLETTER = "auth0-newsletter"
+
 
 class ChimpInsert:
       def __init__(self, audience_id, config, slack_channel_id, slack_api_url, logger):
@@ -36,53 +43,68 @@ class ChimpInsert:
                   if given == member["email_address"]:
                         return member['id']
             return -1
-      
-      def insert_info(self, info):
-            try:
-                  email, fname, lname = info.split(" ")
-            except:
-                  self.logger.info("Bad data received at topic")
-            
-            try:
-                  member_info = {
+
+      def prepare_member_info(self, email, fname, lname):
+            member_info = {
                               "email_address": email,
                               "status":"unsubscribed",
                               "merge_fields" : {
                                     "FNAME":fname,
                                     "LNAME":lname.replace("\n", "")
                   }}
-                  if "auth0-user" in lname:
-                        try:
-                              response = self.mailchimp.lists.add_list_member(self.audience_id, member_info)
-                              self.logger.info("Auth0 user successfully added")
-                              slack_message_resp = requests.post(self.slack_api_url, headers=self.token_header, data=str({"text":f"{email} Auth0 user successfully added"}))
-                        except ApiClientError as error:
-                              return   
-                  elif "auth0-newsletter" in lname:
-                        try:
-                              member_info['status'] = "subscribed"
-                              response = self.mailchimp.lists.add_list_member(self.audience_id, member_info)
-                              self.logger.info(f"{email} Auth0 user with newsletter successfully added")
-                              slack_message_resp = requests.post(self.slack_api_url, headers=self.token_header, data=str({"text":f"{email} Auth0 user with newsletter successfully added"}))
-                        except ApiClientError as error:
-                              error = json.loads(error.text)
-                              if error["title"] == "Member Exists":
-                                    response = self.mailchimp.lists.get_list_members_info(self.audience_id, offset=self.get_offset(self.audience_id))
-                                    user_id = self.get_info(response, email)
-                                    if user_id == -1:
-                                          return
-                                    response = self.mailchimp.update_list_member(self.audience_id, user_id, {"status" : "subscribed"})
-                                    self.logger.info(f"{email} Auth0 user with newsletter successfully added")
-                  elif "stripe" in lname:
-                        response = self.mailchimp.lists.get_list_members_info(self.audience_id, offset=self.get_offset(self.audience_id))
-                        user_id = self.get_info(response, email)
-                        if user_id == -1:
-                              return
-                        response = self.mailchimp.lists.update_list_member_tags(self.audience_id, user_id, {"tags" : [{"name": "Stripe", "status": "active"}]})
-                        self.logger.info(f"{email} Stripe user successfully synchronized")
-                        slack_message_resp = requests.post(self.slack_api_url, headers=self.token_header, data=str({"text":f"{email} Stripe user successfully synchronized"}))
+            return member_info
+      
+      def insert_info(self, info):
+
+            try:
+                  email, fname, lname = info.split(" ")
+            except ValueError:
+                  self.logger.info("ChimpInsert: Bad data received at topic")
+                  return
+            
+            member_info = {}
+
+            try:
+                  member_info = self.prepare_member_info(email, fname, lname)
             except:
-                  self.logger.info("No data received")
+                  self.logger.info("ChimpInsrt: No data received")
+                  return
+
+
+            if TopicInfo.AUTH0.value in lname:
+                  try:
+                        response = self.mailchimp.lists.add_list_member(self.audience_id, member_info)
+                        self.logger.info("ChimpInsert: Auth0 user successfully added")
+                        slack_message_resp = requests.post(self.slack_api_url, headers=self.token_header, data=str({"text":f"{email} Auth0 user successfully added"}))
+                  except ApiClientError as error:
+                        return
+                  
+            
+            elif TopicInfo.A0_NEWSLETTER.value in lname:
+                  try:
+                        member_info['status'] = "subscribed"
+                        response = self.mailchimp.lists.add_list_member(self.audience_id, member_info)
+                        self.logger.info(f"ChimpInsert: {email} Auth0 user with newsletter successfully added")
+                        slack_message_resp = requests.post(self.slack_api_url, headers=self.token_header, data=str({"text":f"{email} Auth0 user with newsletter successfully added"}))
+                  except ApiClientError as error:
+                        error = json.loads(error.text)
+                        if error["title"] == "Member Exists":
+                              response = self.mailchimp.lists.get_list_members_info(self.audience_id, offset=self.get_offset(self.audience_id))
+                              user_id = self.get_info(response, email)
+                              if user_id == -1:
+                                    return
+                              response = self.mailchimp.update_list_member(self.audience_id, user_id, {"status" : "subscribed"})
+                              self.logger.info(f"ChimpInsert: {email} Auth0 user with newsletter successfully added")
+
+            elif TopicInfo.AUTH0.stripe in lname:
+                  response = self.mailchimp.lists.get_list_members_info(self.audience_id, offset=self.get_offset(self.audience_id))
+                  user_id = self.get_info(response, email)
+                  if user_id == -1:
+                        return
+                  response = self.mailchimp.lists.update_list_member_tags(self.audience_id, user_id, {"tags" : [{"name": "Stripe", "status": "active"}]})
+                  self.logger.info(f"ChimpInsert: {email} Stripe user successfully synchronized")
+                  slack_message_resp = requests.post(self.slack_api_url, headers=self.token_header, data=str({"text":f"{email} Stripe user successfully synchronized"}))
+            
 
 async def run(context, input):
       try:
@@ -91,7 +113,7 @@ async def run(context, input):
             audience_id = context.config['audience_id']
             config = {"api_key": context.config['mailchimp_api'], "server": context.config['mailchimp_server']}  
       except Exception as error:
-            raise Exception(f"Config not loaded: {error}")
+            raise Exception(f"ChimpInsert: Config not loaded: {error}")
 
       inserter = ChimpInsert(audience_id, config, slack_channel_id, slack_api_url, context.logger)
       return input.each(inserter.insert_info)
--- python/Stripe/main.py
+++ python/Stripe/main.py
@@ -3,15 +3,18 @@ import stripe
 import asyncio
 from scramjet.streams import Stream
 
+EVENT_REFRESH_DELAY = 3
+
 provides = {
     'provides': 'pipe',
     'contentType': 'text/plain'
 }
 
 class Stripe:
-      def __init__(self,stream,stripe_api):
+      def __init__(self, stream, stripe_api, logger):
             self.stream = stream
             self.stripe_api = stripe_api
+            self.logger = logger
       
       def get_mail(self, user):
             return user['data']['object']['email']
@@ -23,25 +26,29 @@ class Stripe:
             return name
       
       async def get_event(self):
-            await asyncio.sleep(3)
             compared = stripe.Event.list(type="customer.created")['data'][-1]
 
-            self.stream.write(self.get_mail(compared)+ " " +self.get_fullname(compared))
-            compared = compared['id']
-            while True:
-                  test = stripe.Event.list(type="customer.created", ending_before=compared, limit=3)['data']
+            self.logger.info(f"New user in stripe {self.get_mail(compared)}")
+            self.stream.write(self.get_mail(compared)+ " " + self.get_fullname(compared))
 
-                  for i in range(len(test)):
-                        self.stream.write(self.get_mail(test[i]) + " " +self.get_fullname(test[i]))
-                  if len(test) != 0:
-                        compared = test[0]['id']
-                  await asyncio.sleep(3)
+            compared = compared['id']
+
+            while True:
+                  events = stripe.Event.list(type="customer.created", ending_before=compared, limit=3)['data']
+                  
+                  for i in range(len(events)):
+                        self.logger.info(f"New user in stripe {self.get_mail(events[i])}")
+                        self.stream.write(self.get_mail(events[i]) + " " + self.get_fullname(events[i]))
+                  if len(events) != 0:
+                        compared = events[0]['id']
+
+                  await asyncio.sleep(EVENT_REFRESH_DELAY)
 
 async def run(context, input):
       stream = Stream()
       try:
             stripe.api_key = context.config['stripe_api']
-            stripeReader = Stripe(stream, stripe.api_key)
+            stripeReader = Stripe(stream, stripe.api_key, context.logger)
       except Exception as error:
             raise Exception(f"Config not loaded: {error}")
             return
